8486a704e8c43fe53981b852c8eeb144e0e9c8b1
Guilherme Ribeiro de Macedo <guilhermerm99@gmail.com>
2024-09-02 12:03:36 -0300

    Update auth (update_user and delete_user )

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -45,3 +45,32 @@ def login():
         return jsonify({"message": "Login bem-sucedido"}), 200
     else:
         return jsonify({"message": "Senha incorreta"}), 401
+
+@auth_bp.route('/update_user', methods=['PUT'])
+def update_user():
+    data = request.get_json()
+    email = data.get('email')
+    update_data = data.get('update_data')
+
+    user = User(mongo.db)
+    if not user.find_by_email(email):
+        return jsonify({"message": "Usuário não encontrado"}), 404
+
+    if user.update_user(email, update_data):
+        return jsonify({"message": "Usuário atualizado com sucesso"}), 200
+    else:
+        return jsonify({"message": "Erro ao atualizar usuário"}), 500
+
+@auth_bp.route('/delete_user', methods=['DELETE'])
+def delete_user():
+    data = request.get_json()
+    email = data.get('email')
+
+    user = User(mongo.db)
+    if not user.find_by_email(email):
+        return jsonify({"message": "Usuário não encontrado"}), 404
+
+    if user.delete_user(email):
+        return jsonify({"message": "Usuário excluído com sucesso"}), 200
+    else:
+        return jsonify({"message": "Erro ao excluir usuário"}), 500
